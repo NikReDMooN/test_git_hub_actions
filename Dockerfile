7b99259cdbeb282a1c0a7e69ce41fef35f99b13c
nikit <mikitapisarenko1995@gmail.com>
2025-11-17 15:27:17 +0300

    test - Дмитрий Лазебный

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,4 @@ FROM python:3.10-slim
 WORKDIR /app
 COPY . .
 
-RUN pip install pytest
-
 CMD ["python", "app.py"]
--- test_app.py
+++ test_app.py
@@ -1,4 +1,5 @@
 import os
 
 def test_app_file_exists():
+    """Проверяем, что в проекте есть основной файл приложения app.py"""
     assert os.path.exists("app.py"), "Файл app.py не найден!"
