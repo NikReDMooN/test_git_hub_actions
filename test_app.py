4afbc9022a00ca83303292e6be8a2416c00f60cd
nikit <mikitapisarenko1995@gmail.com>
2025-11-17 15:49:28 +0300

    Матвей Гусев

--- test_app.py
+++ test_app.py
@@ -1,11 +1,6 @@
+import pytest
 import os
-import subprocess
 
-def test_app_file_exists():
-    """Тест проверяет наличие файла app.py"""
-    assert os.path.exists("app.py"), "Файл app.py не найден"
-
-def test_app_runs_correctly():
-    """Тест проверяет, что приложение запускается и выводит правильную фразу"""
-    result = subprocess.run(["python", "app.py"], capture_output=True, text=True)
-    assert "it's my workflow" in result.stdout
+def test_app_exists():
+    """Проверка наличия файла app.py"""
+    assert os.path.exists("app.py"), "File app.py not found"
